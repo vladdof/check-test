844486a69a2adcd944c96b1c2f7e243d240d95a2
Vladislav Efanov <vladislav.efanov@gmail.com>
2018-12-02 12:58:24 +0400

    5

--- js/hover.js
+++ js/hover.js
@@ -8,7 +8,7 @@ window.addEventListener("DOMContentLoaded", function() {
 
 		if (isTouchDevice() === true) {	
 
-		    wrapBlock.addEventListener('touchstart', function(event) {
+		    wrapBlock.addEventListener('mouseenter', function(event) {
 		        
 		        let target = event.target;
 		    	event.preventDefault();
